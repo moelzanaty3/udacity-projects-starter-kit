8a90a16ec1938eae8307b4cd22b5730ef8dc692c
Mohammed Elzanaty <mohammedelzanaty129@gmail.com>
2021-05-03 15:25:32 +0200

    Update api.js

--- would-you-rather-game/src/utils/api.js
+++ would-you-rather-game/src/utils/api.js
@@ -1,24 +1,10 @@
-import {
-  _getUsers,
-  _getTweets,
-  _saveLikeToggle,
-  _saveTweet,
-} from './_DATA.js'
+import { _getUsers, _getQuestions } from './_DATA.js'
 
-export function getInitialData () {
-  return Promise.all([
-    _getUsers(),
-    _getTweets(),
-  ]).then(([users, tweets]) => ({
-    users,
-    tweets,
-  }))
-}
-
-export function saveLikeToggle (info) {
-  return _saveLikeToggle(info)
-}
-
-export function saveTweet (info) {
-  return _saveTweet(info)
+export function getInitialData() {
+  return Promise.all([_getUsers(), _getQuestions()]).then(
+    ([users, questions]) => ({
+      users,
+      questions,
+    })
+  )
 }
