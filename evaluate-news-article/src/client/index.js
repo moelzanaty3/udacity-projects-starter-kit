7216a5e5d25fd4f50b33298e9309ff54f07da9e5
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2021-08-31 11:49:26 +0200

    update style, client todos evaluate news article

--- evaluate-news-article/src/client/index.js
+++ evaluate-news-article/src/client/index.js
@@ -1,12 +1,11 @@
 
-// TODO include your scss file here
+// TODO: include your scss file here
 
 // TODO: get the button for submit
 // TODO: add event listener to it when the click to call handleSubmit function
 /**
- * TODO
- *  - Get Value of the input for URL
- *  - Check if it's URL or not
+ * TODO: Get Value of the input for URL
+ *  TODO: Check if it's URL or not
  *      yes
  *          send it to the backend
  *      no
